e8aebf8d67ccc42ae18fe4e9ec7125d6545b13e2
Work <ChinaBackGo@users.noreply.github.com>
2016-03-29 13:45:30 +0800

    modified bokeh test with more example code

--- bokeh-test.py
+++ bokeh-test.py
@@ -7,8 +7,10 @@ y = [6, 7, 2, 4, 5]
 # output to static HTML file
 output_file("airmonitor.html", title="Shinyei Dust Monitor")
 
+TOOLS="resize"
+
 # create a new plot with a title and axis labels
-p = figure(title="Shiney Dust Monitor", x_axis_label='Time', y_axis_label='PM 2.5 Concentration')
+p = figure(tools=TOOLS, title="Shiney Dust Monitor", x_axis_type="datetime", x_axis_label='Time', y_axis_label='PM 2.5 Concentration')
 
 # add a line renderer with legend and line thickness
 p.line(x, y, legend="PM2.5", line_width=2)
