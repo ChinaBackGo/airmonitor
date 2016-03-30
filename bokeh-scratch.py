46b43bef557d685c76a0d184f9adb6a7a88cc115
Work <ChinaBackGo@users.noreply.github.com>
2016-03-30 14:04:52 +0800

    adding bokeh-scratch to show example of using bokeh serve

---
+++ bokeh-scratch.py
@@ -0,0 +1,36 @@
+# myapp.py
+
+import numpy as np
+
+from bokeh.models import Button
+from bokeh.palettes import RdYlBu3
+from bokeh.plotting import figure, curdoc, vplot
+
+# create a plot and style its properties
+p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
+p.border_fill_color = 'black'
+p.background_fill_color = 'black'
+p.outline_line_color = None
+p.grid.grid_line_color = None
+
+# add a text renderer to out plot (no data yet)
+r = p.line(x=[], y=[], color="red")
+
+i = 0
+
+ds = r.data_source
+
+# create a callback that will add a number in a random location
+def callback():
+    global i
+    ds.data['x'].append(i)
+    ds.data['y'].append(np.random.random()*70 + 15)
+    ds.trigger('data', ds.data, ds.data)
+    i = i + 1
+
+# add a button widget and configure with the call back
+button = Button(label="Press Me")
+button.on_click(callback)
+
+# put the button and plot in a layout and add to the document
+curdoc().add_root(vplot(button, p))
